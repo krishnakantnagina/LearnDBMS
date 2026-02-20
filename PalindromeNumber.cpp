811508c75a4b7b637da03cd42156365fb181312f
Krishnakant Nagina <85604967+krishnakantnagina@users.noreply.github.com>
2026-02-20 23:54:18 +0530

    Add palindrome check for a number

---
+++ PalindromeNumber.cpp
@@ -0,0 +1,11 @@
+int n = 121, original = n, rev = 0;
+
+while(n > 0) {
+    rev = rev * 10 + n % 10;
+    n /= 10;
+}
+
+if(original == rev)
+    cout << "Palindrome";
+else
+    cout << "Not Palindrome";
